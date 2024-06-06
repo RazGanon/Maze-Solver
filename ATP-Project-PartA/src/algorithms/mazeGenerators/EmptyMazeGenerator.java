5ca533973578a804fdb003725d2bb82c092ff652
pespes26 <158808321+pespes26@users.noreply.github.com>
2024-06-06 15:19:38 +0300

    Add files via upload

---
+++ ATP-Project-PartA/src/algorithms/mazeGenerators/EmptyMazeGenerator.java
@@ -0,0 +1,9 @@
+package algorithms.mazeGenerators;
+
+public class EmptyMazeGenerator implements AMazeGenerator{
+
+    @Override
+    public Maze generate(int rows, int cols) {
+        return null;
+    }
+}
